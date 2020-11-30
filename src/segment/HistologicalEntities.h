988debd1e3e837d2285bc65d7d6703b74c159fbf
Willian <guns945@gmail.com>
2020-11-30 16:10:06 -0300

    HistologicalEntities stages Python-C Port test.

--- src/segment/HistologicalEntities.h
+++ src/segment/HistologicalEntities.h
@@ -17,118 +17,247 @@
 #include <string.h>
 #include <string>
 
-
 #ifdef _MSC_VER
 #define DllExport __declspec(dllexport)
 #else
-#define DllExport //nothing 
+#define DllExport  // nothing
 #endif
 
 namespace nscale {
 
-
 class DllExport HistologicalEntities {
+  public:
+    static void flushMat(cv::Mat m);
+    static int showMat(cv::Mat m);
 
-public:
+    static const int BACKGROUND = 1;
+    static const int BACKGROUND_LIKELY = 2;
+    static const int NO_CANDIDATES_LEFT = 3;
+    static const int INVALID_IMAGE = -1;
+    static const int SUCCESS = 0;
+    static const int CONTINUE = 4;
+    static const int RUNTIME_FAILED = -2;
 
-	static void flushMat(cv::Mat m);
-	static int showMat(cv::Mat m);
+    static cv::Mat getRBC(
+        const cv::Mat &img, double T1 = 5.0, double T2 = 4.0,
+        ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
+    static cv::Mat getRBC(
+        const std::vector<cv::Mat> &bgr, double T1 = 5.0, double T2 = 4.0,
+        ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
+    static cv::Mat getBackground(
+        const cv::Mat &img, unsigned char blue = 220, unsigned char green = 220,
+        unsigned char red = 220, ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
+    static cv::Mat getBackground(
+        const std::vector<cv::Mat> &bgr, unsigned char blue = 220,
+        unsigned char green = 220, unsigned char red = 220,
+        ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
 
+    static int segmentNuclei(
+        const cv::Mat &img, cv::Mat &mask, int &compcount, int *&bbox,
+        unsigned char blue = 220, unsigned char green = 220,
+        unsigned char red = 220, double T1 = 5.0, double T2 = 4.0,
+        unsigned char G1 = 80, int minSize = 11, int maxSize = 1000,
+        unsigned char G2 = 45, int minSizePl = 30, int minSizeSeg = 21,
+        int maxSizeSeg = 1000, int fillHolesConnectivity = 4,
+        int reconConnectivity = 8, int watershedConnectivity = 8,
+        ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
+    static int segmentNuclei(
+        const std::string &input, const std::string &output, int &compcount,
+        int *&bbox, unsigned char blue = 220, unsigned char green = 220,
+        unsigned char red = 220, double T1 = 5.0, double T2 = 4.0,
+        unsigned char G1 = 80, int minSize = 11, int maxSize = 1000,
+        unsigned char G2 = 45, int minSizePl = 30, int minSizeSeg = 21,
+        int maxSizeSeg = 1000, int fillHolesConnectivity = 4,
+        int reconConnectivity = 8, int watershedConnectivity = 8,
+        ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
 
-	static const int BACKGROUND = 1;
-	static const int BACKGROUND_LIKELY = 2;
-	static const int NO_CANDIDATES_LEFT = 3;
-	static const int INVALID_IMAGE = -1;
-	static const int SUCCESS = 0;
-	static const int CONTINUE = 4;
-	static const int RUNTIME_FAILED = -2;
+    static int segmentNuclei(
+        const cv::Mat &img, cv::Mat &mask, unsigned char blue = 220,
+        unsigned char green = 220, unsigned char red = 220, double T1 = 5.0,
+        double T2 = 4.0, unsigned char G1 = 80, int minSize = 11,
+        int maxSize = 1000, unsigned char G2 = 45, int minSizePl = 30,
+        int minSizeSeg = 21, int maxSizeSeg = 1000,
+        int fillHolesConnectivity = 4, int reconConnectivity = 8,
+        int watershedConnectivity = 8,
+        ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
 
+    static int segmentNucleiStg1(
+        const cv::Mat &img, unsigned char blue, unsigned char green,
+        unsigned char red, double T1, double T2, std::vector<cv::Mat> *bgr,
+        cv::Mat *rbc, ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
+    static int segmentNucleiStg2(
+        int reconConnectivity, std::vector<cv::Mat> *bgr, cv::Mat *rc,
+        cv::Mat *rc_recon, cv::Mat *rc_open,
+        ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
+    static int segmentNucleiStg3(
+        int fillHolesConnectivity, unsigned char G1, cv::Mat *rc,
+        cv::Mat *rc_recon, cv::Mat *rc_open, cv::Mat *bw1, cv::Mat *diffIm,
+        ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
+    static int segmentNucleiStg4(
+        int minSize, int maxSize, cv::Mat *bw1, cv::Mat *bw1_t,
+        ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
+    static int segmentNucleiStg5(
+        unsigned char G2, cv::Mat *diffIm, cv::Mat *bw1_t, cv::Mat *rbc,
+        cv::Mat *seg_open, ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
+    static int segmentNucleiStg6(
+        const cv::Mat &img, int minSizePl, int watershedConnectivity,
+        cv::Mat *seg_open, cv::Mat *seg_nonoverlap,
+        ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
+    static int segmentNucleiStg7(
+        cv::Mat *output, int minSizeSeg, int maxSizeSeg,
+        int fillHolesConnectivity, cv::Mat *seg_nonoverlap,
+        ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
 
-	static cv::Mat getRBC(const cv::Mat& img, double T1=5.0, double T2=4.0,
-			::cciutils::SimpleCSVLogger *logger = NULL, ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
-	static cv::Mat getRBC(const std::vector<cv::Mat>& bgr, double T1=5.0, double T2=4.0,
-			::cciutils::SimpleCSVLogger *logger = NULL, ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
-	static cv::Mat getBackground(const cv::Mat& img, unsigned char blue=220, unsigned char green=220, unsigned char red=220,
-			::cciutils::SimpleCSVLogger *logger = NULL, ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
-	static cv::Mat getBackground(const std::vector<cv::Mat>& bgr, unsigned char blue=220, unsigned char green=220, unsigned char red=220,
-			::cciutils::SimpleCSVLogger *logger = NULL, ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
-
-	static int segmentNuclei(const cv::Mat& img, cv::Mat& mask,
-			int &compcount, int *&bbox, unsigned char blue=220, unsigned char green=220, unsigned char red=220, double T1=5.0, double T2=4.0, unsigned char G1 = 80, int minSize=11, int maxSize=1000, unsigned char G2 = 45, int minSizePl=30, int minSizeSeg=21, int maxSizeSeg=1000,  int fillHolesConnectivity=4, int reconConnectivity=8, int watershedConnectivity=8,
-			::cciutils::SimpleCSVLogger *logger = NULL, ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
-	static int segmentNuclei(const std::string& input, const std::string& output,
-			int &compcount, int *&bbox, unsigned char blue=220, unsigned char green=220, unsigned char red=220, double T1=5.0, double T2=4.0, unsigned char G1 = 80, int minSize=11, int maxSize=1000, unsigned char G2 = 45, int minSizePl=30, int minSizeSeg=21, int maxSizeSeg=1000,  int fillHolesConnectivity=4, int reconConnectivity=8, int watershedConnectivity=8,
-			::cciutils::SimpleCSVLogger *logger = NULL, ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
-
-	static int segmentNuclei(const cv::Mat& img, cv::Mat& mask, unsigned char blue=220, unsigned char green=220, unsigned char red=220, double T1=5.0, double T2=4.0, unsigned char G1 = 80, int minSize=11, int maxSize=1000, unsigned char G2 = 45, int minSizePl=30, int minSizeSeg=21, int maxSizeSeg=1000, int fillHolesConnectivity=4, int reconConnectivity=8, int watershedConnectivity=8,
-			::cciutils::SimpleCSVLogger *logger = NULL, ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
-
-	static int segmentNucleiStg1(const cv::Mat& img, unsigned char blue, unsigned char green, unsigned char red, double T1, 
-			double T2, std::vector<cv::Mat> *bgr, cv::Mat* rbc, ::cciutils::SimpleCSVLogger *logger = NULL, 
-			::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
-	static int segmentNucleiStg2(int reconConnectivity, std::vector<cv::Mat> *bgr, cv::Mat* rc, cv::Mat* rc_recon, cv::Mat *rc_open,
-			::cciutils::SimpleCSVLogger *logger = NULL, ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
-	static int segmentNucleiStg3(int fillHolesConnectivity, unsigned char G1, cv::Mat* rc, cv::Mat* rc_recon, cv::Mat *rc_open, 
-			cv::Mat *bw1, cv::Mat* diffIm, ::cciutils::SimpleCSVLogger *logger = NULL, 
-			::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
-	static int segmentNucleiStg4(int minSize, int maxSize, cv::Mat* bw1, cv::Mat* bw1_t, 
-			::cciutils::SimpleCSVLogger *logger = NULL, ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
-	static int segmentNucleiStg5(unsigned char G2, cv::Mat* diffIm, cv::Mat* bw1_t, cv::Mat* rbc, cv::Mat* seg_open, 
-			::cciutils::SimpleCSVLogger *logger = NULL, ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
-	static int segmentNucleiStg6(const cv::Mat& img, int minSizePl, int watershedConnectivity, cv::Mat* seg_open, 
-			cv::Mat* seg_nonoverlap, ::cciutils::SimpleCSVLogger *logger = NULL, 
-			::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
-	static int segmentNucleiStg7(cv::Mat* output, int minSizeSeg, int maxSizeSeg, int fillHolesConnectivity, cv::Mat* seg_nonoverlap, 
-			::cciutils::SimpleCSVLogger *logger = NULL, 
-			::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
-
-	// the following are specific to the task based implementation for HPDC paper.  The pipeline is refactoring into this form so we're maintaining one set of code.
-	static int plFindNucleusCandidates(const cv::Mat& img, cv::Mat& seg_norbc, unsigned char blue=220, unsigned char green=220, unsigned char red=220, double T1=5.0, double T2=4.0, unsigned char G1 = 80, int minSize=11, int maxSize=1000, unsigned char G2 = 45, int fillHolesConnectivity=4, int reconConnectivity=8,
-			::cciutils::SimpleCSVLogger *logger = NULL, ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);  // S1
-	static int plSeparateNuclei(const cv::Mat& img, const cv::Mat& seg_open, cv::Mat& seg_nonoverlap, int minSizePl=30, int watershedConnectivity=8,
-			::cciutils::SimpleCSVLogger *logger = NULL, ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL); // A4
-
+    // the following are specific to the task based implementation for HPDC
+    // paper.  The pipeline is refactoring into this form so we're maintaining
+    // one set of code.
+    static int plFindNucleusCandidates(
+        const cv::Mat &img, cv::Mat &seg_norbc, unsigned char blue = 220,
+        unsigned char green = 220, unsigned char red = 220, double T1 = 5.0,
+        double T2 = 4.0, unsigned char G1 = 80, int minSize = 11,
+        int maxSize = 1000, unsigned char G2 = 45,
+        int fillHolesConnectivity = 4, int reconConnectivity = 8,
+        ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);  // S1
+    static int plSeparateNuclei(
+        const cv::Mat &img, const cv::Mat &seg_open, cv::Mat &seg_nonoverlap,
+        int minSizePl = 30, int watershedConnectivity = 8,
+        ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);  // A4
 };
-
 
 #ifdef WITH_CUDA
 namespace gpu {
 
 class DllExport HistologicalEntities {
+  public:
+    static cv::gpu::GpuMat getRBC(
+        const std::vector<cv::gpu::GpuMat> &bgr, cv::gpu::Stream &stream,
+        ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
+    static cv::gpu::GpuMat getBackground(
+        const std::vector<cv::gpu::GpuMat> &bgr, cv::gpu::Stream &stream,
+        ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
 
-public:
+    static int segmentNuclei(
+        const cv::gpu::GpuMat &img, cv::gpu::GpuMat &mask, int &compcount,
+        int *&g_bbox, cv::gpu::Stream *str = NULL,
+        ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
+    static int segmentNuclei(
+        const cv::Mat &img, cv::Mat &mask, int &compcount, int *&bbox,
+        cv::gpu::Stream *str = NULL, ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
+    static int segmentNuclei(
+        const std::string &input, const std::string &output, int &compcount,
+        int *&bbox, cv::gpu::Stream *str = NULL,
+        ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
 
-	static cv::gpu::GpuMat getRBC(const std::vector<cv::gpu::GpuMat>& bgr, cv::gpu::Stream& stream,
-			::cciutils::SimpleCSVLogger *logger = NULL, ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
-	static cv::gpu::GpuMat getBackground(const std::vector<cv::gpu::GpuMat>& bgr, cv::gpu::Stream& stream,
-			::cciutils::SimpleCSVLogger *logger = NULL, ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
-
-	static int segmentNuclei(const cv::gpu::GpuMat& img, cv::gpu::GpuMat& mask,
-			int &compcount, int *&g_bbox, cv::gpu::Stream *str = NULL,
-			::cciutils::SimpleCSVLogger *logger = NULL, ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
-	static int segmentNuclei(const cv::Mat& img, cv::Mat& mask,
-			int &compcount, int *&bbox, cv::gpu::Stream *str = NULL,
-			::cciutils::SimpleCSVLogger *logger = NULL, ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
-	static int segmentNuclei(const std::string& input, const std::string& output,
-			int &compcount, int *&bbox, cv::gpu::Stream *str = NULL,
-			::cciutils::SimpleCSVLogger *logger = NULL, ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
-
-	// the following are specific to the task based implementation for HPDC paper.  The pipeline is refactoring into this form so we're maintaining one set of code.
-	static int plFindNucleusCandidates(const cv::gpu::GpuMat& g_img, cv::gpu::GpuMat& g_seg_norbc, cv::gpu::Stream& stream,
-			::cciutils::SimpleCSVLogger *logger = NULL, ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);  // S1
-	static int plSeparateNuclei(const cv::gpu::GpuMat& g_img, cv::gpu::GpuMat& g_seg_open, cv::gpu::GpuMat& g_seg_nonoverlap, cv::gpu::Stream& stream,
-			::cciutils::SimpleCSVLogger *logger = NULL, ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL); // A4
-
-
+    // the following are specific to the task based implementation for HPDC
+    // paper.  The pipeline is refactoring into this form so we're maintaining
+    // one set of code.
+    static int plFindNucleusCandidates(
+        const cv::gpu::GpuMat &g_img, cv::gpu::GpuMat &g_seg_norbc,
+        cv::gpu::Stream &stream, ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);  // S1
+    static int plSeparateNuclei(
+        const cv::gpu::GpuMat &g_img, cv::gpu::GpuMat &g_seg_open,
+        cv::gpu::GpuMat &g_seg_nonoverlap, cv::gpu::Stream &stream,
+        ::cciutils::SimpleCSVLogger *logger = NULL,
+        ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);  // A4
 };
 
-DllExport int* boundingBox2(cv::gpu::GpuMat g_input, cv::gpu::Stream *str);
-DllExport int* boundingBox2(cv::gpu::GpuMat g_input, int &compcount, cv::gpu::Stream *str);
+DllExport int *boundingBox2(cv::gpu::GpuMat g_input, cv::gpu::Stream *str);
+DllExport int *boundingBox2(cv::gpu::GpuMat g_input, int &compcount,
+                            cv::gpu::Stream *str);
 DllExport void cudaFreeData(char *dataPtr);
 DllExport void cudaDownloadData(char *dest, char *from, int size);
 
-}
+}  // namespace gpu
 #endif
 
+}  // namespace nscale
+
+#ifdef WITH_PYTHON_PORT
+#include <Python.h>
+
+static PyObject *segmentNucleiStg1Py(PyObject *self, PyObject *args) {
+    // Parse arguments
+    // cv::Mat &img;
+    unsigned char blue;
+    unsigned char green;
+    unsigned char red;
+    double T1;
+    double T2;
+    std::vector<cv::Mat> *bgr;
+    cv::Mat *rbc;
+
+    if (!PyArg_ParseTuple(args, "bbbdd", &blue, &green, &red, &T1, &T2)) {
+        return NULL;
+    }
+
+    std::cout << "Read parameters " << blue << ", " << green << ", " << red
+              << ", " << T1 << ", " << T2 << ", "
+              << " and will return zero\n";
+
+    return PyLong_FromLong(0);
 }
+
+static PyMethodDef FputsMethods[] = {
+    {"segmentNucleiStg1", segmentNucleiStg1Py, METH_VARARGS,
+     "segmentNucleiStg1 C function"},
+    {NULL, NULL, 0, NULL}};
+
+static struct PyModuleDef fputsmodule = {PyModuleDef_HEAD_INIT, "nscale",
+                                         "nscale C module", -1, FputsMethods};
+
+PyMODINIT_FUNC PyInit_fputs(void) { return PyModule_Create(&fputsmodule); }
+
+#endif
+
+// static int segmentNucleiStg2(
+//     int reconConnectivity, std::vector<cv::Mat> *bgr, cv::Mat *rc,
+//     cv::Mat *rc_recon, cv::Mat *rc_open,
+//     ::cciutils::SimpleCSVLogger *logger = NULL,
+//     ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
+// static int segmentNucleiStg3(
+//     int fillHolesConnectivity, unsigned char G1, cv::Mat *rc, cv::Mat
+//     *rc_recon, cv::Mat *rc_open, cv::Mat *bw1, cv::Mat *diffIm,
+//     ::cciutils::SimpleCSVLogger *logger = NULL,
+//     ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
+// static int segmentNucleiStg4(
+//     int minSize, int maxSize, cv::Mat *bw1, cv::Mat *bw1_t,
+//     ::cciutils::SimpleCSVLogger *logger = NULL,
+//     ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
+// static int segmentNucleiStg5(
+//     unsigned char G2, cv::Mat *diffIm, cv::Mat *bw1_t, cv::Mat *rbc,
+//     cv::Mat *seg_open, ::cciutils::SimpleCSVLogger *logger = NULL,
+//     ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
+// static int segmentNucleiStg6(
+//     const cv::Mat &img, int minSizePl, int watershedConnectivity,
+//     cv::Mat *seg_open, cv::Mat *seg_nonoverlap,
+//     ::cciutils::SimpleCSVLogger *logger = NULL,
+//     ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
+// static int segmentNucleiStg7(
+//     cv::Mat *output, int minSizeSeg, int maxSizeSeg, int
+//     fillHolesConnectivity, cv::Mat *seg_nonoverlap,
+//     ::cciutils::SimpleCSVLogger *logger = NULL,
+//     ::cciutils::cv::IntermediateResultHandler *iresHandler = NULL);
+
 #endif /* HistologicalEntities_H_ */
